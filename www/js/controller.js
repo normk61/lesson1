57420a5e148745169155a4b3360128b4e6a1743d
Norman Katz <normkatz1@gmail.com>
2016-03-22 19:30:29 +0000

    finished lesson 3

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,5 +1,5 @@
 angular.module('starter.controllers', [])
-.controller('LoginCtrl',['$scope', '$state', 'UserService', '$ionicHistory', function($scope, $state, UserService, $ionicHistory) {
+.controller('LoginCtrl',['$scope', '$state', 'UserService', '$ionicHistory', '$window', function($scope, $state, UserService, $ionicHistory, $window) {
     $scope.user = {};
     $scope.loginSubmitForm = function(form)
     {
@@ -10,6 +10,8 @@ angular.module('starter.controllers', [])
                 if (response.status === 200) {
                     //Should return a token
                     console.log(response);
+                    $window.localStorage["userID"] = response.data.userId;
+                    $window.localStorage['token'] = response.data.id;
                     $ionicHistory.nextViewOptions({
                       historyRoot: true,
                       disableBack: true
@@ -34,7 +36,7 @@ angular.module('starter.controllers', [])
         }
     };
 }])
-.controller('RegisterCtrl',['$scope', '$state', 'UserService', '$ionicHistory', function($scope, $state, UserService, $ionicHistory) {
+.controller('RegisterCtrl',['$scope', '$state', 'UserService', '$ionicHistory', '$window', function($scope, $state, UserService, $ionicHistory, $window) {
     $scope.repeatPwd = {};
     $scope.user = {};
     $scope.registerSubmitForm = function(form)
@@ -55,6 +57,9 @@ angular.module('starter.controllers', [])
                       historyRoot: true,
                       disableBack: true
                     });
+                    
+                    loginAfterRegister();
+                    
                     $state.go('lobby');
                 } else {
                     // invalid response
@@ -74,6 +79,121 @@ angular.module('starter.controllers', [])
             });
         }
     };
+    
+     //Required to get the access token
+    function loginAfterRegister()
+    {
+        UserService.login($scope.user)
+        .then(function(response) {
+            if (response.status === 200) {
+                //Should return a token
+                $window.localStorage["userID"] = response.data.userId;
+                $window.localStorage['token'] = response.data.id;
+                $ionicHistory.nextViewOptions({
+                   historyRoot: true,
+                   disableBack: true
+                });
+                $state.go('lobby');
+            } else {
+                // invalid response
+                $state.go('landing');
+            }
+        }, function(response) {
+            // something went wrong
+            console.log(response);
+            $state.go('landing');
+        });
+    };
+    
+}])
+.controller('LobbyCtrl',['$scope', '$state', '$ionicHistory', 'UserService', '$window', 'ServerQuestionService', 'TKQuestionsService',
+  function($scope, $state, $ionicHistory, UserService, $window, ServerQuestionService, TKQuestionsService) {
+    $scope.logout = function()
+    {
+         UserService.logout($window.localStorage.token)
+         .then(function(response) {
+            //The successful code for logout is 204
+            if(response.status === 204)
+            {
+                $ionicHistory.nextViewOptions({
+                  historyRoot: true,
+                  disableBack: true
+                });
+                $state.go('landing');
+            }else {
+                 alert("Could not logout at this moment, try again.");
+            }
+        }, function(response) {
+            alert("Could not logout at this moment, try again.");
+        });
+    };
+    
+    //Get Questions Initially if they are not already stored
+    if(TKQuestionsService.questionsLength() === 0)
+        getQuestions();
+     
+    function getQuestions()
+    {
+        ServerQuestionService.all($window.localStorage['token'])
+        .then(function(response) {
+            if (response.status === 200) {
+                var questions = response.data;
+                TKQuestionsService.setQuestions(questions);
+            } else {
+                // invalid response
+                confirmPrompt();
+            }
+        }, function(response) {
+            // something went wrong
+            confirmPrompt();
+        });
+    }
+   
+    function confirmPrompt()
+    {
+        var response = confirm("The questions could not be retrieved at this time, do you want to try again?");
+        if (response == true) {
+            getQuestions();
+        }
+    }
+    $scope.takeTestButtonTapped = function()
+    {
+        if(TKQuestionsService.questionsLength() === 0)
+            getQuestions();
+        else {
+           $state.go('test.detail',{testID:1});
+        }
+    };
+    
+}])
+.controller('TestCtrl', ['$scope', 'testInfo', '$stateParams', '$state',
+    function($scope, testInfo, $stateParams, $state) {
+        //testInfo is passed in the router to obtain the questions
+        var qNumber = $stateParams.testID;
+        $scope.title = "Question #"+qNumber;
+        testInfo.forEach(function(infoDict)
+        {
+            if(infoDict.Answer_ID === "A") {
+                $scope.questionA = infoDict;
+            } else if(infoDict.Answer_ID === "B") {
+                $scope.questionB = infoDict;
+            }
+        });
+        $scope.buttonClicked = function ( option ) {
+            if(option === "A") {
+                console.log("Chose A");
+            }
+            else if(option === "B") {
+                console.log("Chose B");
+            }
+            var nextqNumber = Number(qNumber) +1;
+            if(nextqNumber>30) {
+                $state.go('results');
+            }else {
+                $state.go('test.detail',{testID:nextqNumber});
+            }
+        };
 }]);
+
 
 
